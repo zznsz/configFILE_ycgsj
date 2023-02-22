d6b16310609bef2bf7e9366dcc2125204af64409
zznsz <78538980+zznsz@users.noreply.github.com>
2023-02-22 20:54:32 +0800

    Update autoConfig.py

--- autoConfig.py
+++ autoConfig.py
@@ -307,11 +307,11 @@ class BasicTestCase(unittest.TestCase):
 
      # email config 
     def test_register_params_check_ok2(self):
-        content = {'建湖县人民政府行政审批局'}
+        content = {'滨海县人民政府行政审批局'}
         content['username'] = 'wahaha'
         content['password'] = 'abcd1234'
         content['nickname'] = 'wangluli'
-        content['domain'] = 'http://www.jianhu.gov.cn/'
+        content['domain'] = 'http://binhai.yancheng.gov.cn/'
         content['email'] = 'p3rfectVICT1M@163.com'
         content['mobile'] = '+86.18661209709'
         self.assertEqual(register_params_check(content), ("ok", True))
